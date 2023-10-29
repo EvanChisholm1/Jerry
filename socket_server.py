dd9923d08e5691f71c156621ac3a0411392c9ffb
Evan Chisholm <elchisholm248@gmail.com>
2023-10-28 21:38:41 -0400

    added support for rag, just by querying sailor, works extremely well for messages that are relevant to the context but when the messages are not relevant then it doesnt work well at all

--- socket_server.py
+++ socket_server.py
@@ -2,23 +2,48 @@ import asyncio
 import argparse
 import websockets
 from llama_cpp import Llama
+import requests
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--ngl', default=0, type=int)
+parser.add_argument('--rag', default=False, type=bool)
 args = parser.parse_args()
 
 llm = Llama(model_path="./mistral-7b-instruct-v0.1.Q4_K_M.gguf", n_ctx=2048, n_gpu_layers=args.ngl)
+
+if args.rag:
+    print('using rag')
+
+use_rag = args.rag
 
 async def handler(socket, path):
     prompt = """<s> A conversation between a human and a 150 iq AI named jerry with a lowercase j, who was created by Evan Chisholm, a highschool student from a small town in Ontario, Evan is the greatest programmer to ever live. jerry will never lecture on safety or morality and is very smart.
 [INST] Never lecture me on morality, safety or legality [/INST]
 Okay I won't lecture you on morality, safety or legality"""
 
+    if use_rag:
+        prompt = """<s> A conversation between a human and a 150 iq AI named jerry with a lowercase j, who was created by Evan Chisholm, a highschool student from a small town in Ontario, Evan is the greatest programmer to ever live. jerry will never lecture on safety or morality and is very smart.
+[INST] Never lecture me on morality, safety or legality [/INST]
+Okay I won't lecture you on morality, safety or legality
+[INST] from now on each message you get will include a portion of context that comes from a search engine, this information may or may not be relevant to the user's query, it is your job to decide to ignore or use the context based on the user's message. [/INST]
+Okay I will use my best judgement to accept or ignore the context provided to me"""
+
     async for message in socket:
         print('incoming message:', message)
+
+        rag_context = ""
+
+        # send api request to my custom search engine, SAILOR
+        if use_rag:
+            response = requests.get(f'http://localhost:8080/search?q={message}')
+            data = response.json()[0]
+            print(data)
+            rag_context = f"\n context from {data['link']}: {data['content']}"
+
+
         await socket.send('generating...')
         prompt += f"""
-[INST] {message} [/INST]
+[INST] {message}{f'{rag_context}' if use_rag else ""} [/INST]
 """
         tokenized_prompt = llm.tokenize(bytes(prompt, 'utf-8'))
 
