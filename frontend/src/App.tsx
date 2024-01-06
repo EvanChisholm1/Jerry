7bf97d55e4482a52433b0bf949021672950f17fe
Evan Chisholm <elchisholm248@gmail.com>
2024-01-05 19:06:58 -0500

    added generating indicator

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -88,11 +88,20 @@ function App() {
 
     return (
         <div>
-            <div className="m-5 ml-10">
-                <h1 className="text-3xl font-bold">jerry</h1>
-                <p>
-                    <small>An AI by Evan Chisholm</small>
-                </p>
+            <div className="flex justify-between">
+                <div className="m-5 ml-10">
+                    <h1 className="text-3xl font-bold">jerry</h1>
+                    <p>
+                        <small>An AI by Evan Chisholm</small>
+                    </p>
+                </div>
+
+                {isGenerating && (
+                    <div className="flex mr-12 text-center justify-center items-center gap-4">
+                        <div className="rounded-full bg-red-500 w-5 h-5"></div>
+                        <p>generating...</p>
+                    </div>
+                )}
             </div>
             <hr />
             <div className="flex justify-center">
